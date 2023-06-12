578278501b265e69f5882549afe84f7f414fcae2
김정환 <jhkim8761@hanmail.net>
2023-06-12 18:02:23 +0900

    스프링부트 커밋

---
+++ src/main/java/com/startup/disco/DiscoApplication.java
@@ -0,0 +1,13 @@
+package com.startup.disco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoApplication.class, args);
+	}
+
+}
