d251b4fd4a7f96d245f255db1c5ba2447ffd3fe2
bbqgo <bbgoood@nate.com>
2023-07-05 19:47:11 +0900

    오류 수정 ...

--- src/main/java/com/startup/disco/repository/ProductRepository.java
+++ src/main/java/com/startup/disco/repository/ProductRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductDTO, String> {
-    List<ProductDTO> findByProductId(String productId);
+    List<ProductDTO> findByProductCd(String productId);
 }
